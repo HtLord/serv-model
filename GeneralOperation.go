b60ea1882b346f8c535683f05bb8876396f1347c
HtLord <s75757775@hotmail.com>
2019-05-14 17:03:25 +0800

    Modify Roles model and fit gin framework.

---
+++ GeneralOperation.go
@@ -0,0 +1,11 @@
+package servmodel
+
+import "github.com/gin-gonic/gin"
+
+func Wrapper(obj *interface{}, c gin.Context) error {
+	err := c.ShouldBind(obj)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ Roles.go
@@ -0,0 +1,30 @@
+package servmodel
+
+import (
+	"time"
+)
+
+type ADMLevel int
+
+const (
+	ROOT_ADM ADMLevel = 0
+)
+
+type Member struct {
+	Name string `json: "name" binding: "required"`
+	Email string `json: "email" binding: "required"`
+	Secret string `json: "secret" binding: "required"`
+	CreateTime time.Time `json: "createTime" binding: "required"`
+	UpdateTime time.Time `json: "updateTime" binding: "required"`
+}
+
+type ADM struct {
+	Member
+	Level ADMLevel
+}
+
+type Store struct {
+	BAN string `json: "ban" binding: "required"`
+	ADMs []ADM `json: "adms" binding: "required"`
+	Members []Member `json: "members" binding: "required"`
+}
